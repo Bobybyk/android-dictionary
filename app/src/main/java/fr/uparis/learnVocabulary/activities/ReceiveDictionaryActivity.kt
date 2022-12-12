2b3aca8b7e6e3d1610473edcb1572fee6c39ef43
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-12-12 15:45:09 +0100

    réception lien dictionnaire depuis navigateur

---
+++ app/src/main/java/fr/uparis/learnVocabulary/activities/ReceiveDictionaryActivity.kt
@@ -0,0 +1,26 @@
+package fr.uparis.learnVocabulary.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import fr.uparis.learnVocabulary.databinding.ActivityReceiveDictionaryBinding
+
+class ReceiveDictionaryActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityReceiveDictionaryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityReceiveDictionaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(intent.action.equals( "android.intent.action.SEND" )){
+            val txt = intent.extras?.getString( "android.intent.extra.TEXT" )
+            if (txt != null) {
+                Log.i("ReceiveDictionnaryActivity", txt)
+            }
+        }
+    }
+}
