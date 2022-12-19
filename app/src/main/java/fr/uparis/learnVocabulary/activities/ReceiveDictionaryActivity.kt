f75d194ea7a040ec4cefc272bded15286998ac99
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-12-19 17:35:36 +0100

    link DictionaryActivity to db

--- app/src/main/java/fr/uparis/learnVocabulary/activities/ReceiveDictionaryActivity.kt
+++ app/src/main/java/fr/uparis/learnVocabulary/activities/ReceiveDictionaryActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import fr.uparis.learnVocabulary.database.entities.Dictionary
+import fr.uparis.learnVocabulary.database.entities.Language
 import fr.uparis.learnVocabulary.databinding.ActivityReceiveDictionaryBinding
+import fr.uparis.learnVocabulary.viewModels.ManageLanguagesViewModel
+import fr.uparis.learnVocabulary.viewModels.ReceiveDictionaryViewModel
 
 class ReceiveDictionaryActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityReceiveDictionaryBinding
+
+    val model by lazy {
+        ViewModelProvider(this)[ReceiveDictionaryViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
@@ -20,7 +30,35 @@ class ReceiveDictionaryActivity : AppCompatActivity() {
             val txt = intent.extras?.getString( "android.intent.extra.TEXT" )
             if (txt != null) {
                 Log.i("ReceiveDictionnaryActivity", txt)
+                
             }
         }
+
+        //update list after dictionary insert query
+        model.insertInfo.observe(this) {
+            if(it.equals(0))
+                return@observe
+            else if(it.equals(-1))
+                notifyError()
+            model.loadAllDictionaries()
+        }
+
+        //update list after deleting dictionary
+        model.deleteInfo.observe(this) {
+            if(it.equals(0))
+                return@observe
+            else if(it.equals(-1))
+                notifyError()
+            model.loadAllDictionaries()
+        }
+    }
+
+    fun cutRoutes(url: String): List<String> {
+        val strArray = mutableListOf<String>()
+        return url.split(':')
+    }
+
+    fun notifyError() {
+        Toast.makeText(this,"Une erreur est survenue !", Toast.LENGTH_SHORT).show()
     }
 }
